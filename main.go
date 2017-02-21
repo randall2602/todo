223b8a81f60521039868009c5d4064396b49a850
Randall Sturgis <randallcsturgis@gmail.com>
2017-02-21 01:47:17 -0800

    implement sql

--- main.go
+++ main.go
@@ -2,63 +2,58 @@
 package main
 
 import (
-	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
 
-	// Imports the Google Cloud Datastore client package.
-	"cloud.google.com/go/datastore"
-	"golang.org/x/net/context"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type database struct {
-	projectID string
-	ctx       context.Context
-	client    *datastore.Client
+	conn *sql.DB
+	user string
+	pass string
+	host string
+	port string
+	db   string
 }
 
-func (db *database) Init(projectID string) {
-	db.projectID = projectID
-	db.ctx = context.Background()
+func (db *database) Init(user, pass, host, port, use string) error {
 	var err error
-	db.client, err = datastore.NewClient(db.ctx, db.projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-}
-
-type task struct {
-	Description string
-}
-
-func (db *database) StoreTask(kind, name string, task task) *datastore.Key {
-	key := datastore.NameKey(kind, name, nil)
-	if _, err := db.client.Put(db.ctx, key, &task); err != nil {
-		log.Fatalf("Failed to save task: %v", err)
-	}
-	return key
+	driver := "mysql"
+	protocol := "tcp"
+	connString := user + ":" + pass + "@" + protocol + "(" + host + ":" + port + ")/" + use
+	db.conn, err = sql.Open(driver, connString)
+	return err
 }
 
 func main() {
+	// set up database
 	var db database
-	db.Init("alert-height-150418")
-	scanner := bufio.NewScanner(os.Stdin)
-	kind := "Task"
-	taskCounter := 0
-	fmt.Print("q to quit; enter first task: ")
-	for scanner.Scan() {
-		if scanner.Text() == "quit" {
-			break
-		}
-		description := scanner.Text()
-		taskCounter++
-		name := "task" + strconv.Itoa(taskCounter)
-		task := task{
-			Description: description,
-		}
-		fmt.Printf("Saved %v\n", db.StoreTask(kind, name, task))
-		fmt.Print("enter next task: ")
+	err := db.Init("app", "NWlmeA27jk", "127.0.0.1", "3306", "todo")
+	if err != nil {
+		log.Fatal(err)
 	}
+	// run a query
+	var (
+		name        string
+		description string
+	)
+	rows, err := db.conn.Query("select name, description from tasks_summary where completed=?", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&name, &description)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("list: ", name, "\ttask: ", description)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.conn.Close()
 }
